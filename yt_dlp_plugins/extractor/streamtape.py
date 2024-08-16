e21c7fa64f0bd3e34382f09a141b2419e8ef506d
dbiesecke <dbiesecke@users.noreply.github.com>
2024-08-16 23:53:40 +0200

    Update streamtape.py
    Fix code

--- yt_dlp_plugins/extractor/streamtape.py
+++ yt_dlp_plugins/extractor/streamtape.py
@@ -1,18 +1,13 @@
-from __future__ import unicode_literals
+import re
+
+#from __future__ import unicode_literals
 
 from yt_dlp.extractor.common import InfoExtractor
 from yt_dlp.utils import js_to_json, urljoin
 
-# strings are obfuscated by concatenating substrings
-split_string_part = r'(?:%s|%s)' % (r'"(?:[^"\\]|\\.)*"',
-                                    r"'(?:[^'\\]|\\.)*'")
-split_string = r'(?:' + split_string_part + r'(?:\s*\+\s*' + split_string_part + r')*)'
-videolink = r"(?:'\+')?".join('videolink')
-videolink = r"document\.getElementById\('" + videolink + r"'\)\.innerHTML\s*=\s*(?P<data>" + split_string + r")"
-
 
 class StreamtapeIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?streamtape.to/[ev]/(?P<id>[^/?#]+)'
+    _VALID_URL = r'https?://(?:www\.)?streamtape.[comt]+/[ev]/(?P<id>[^/?#]+)'
     _TESTS = [{
         'url': 'https://streamtape.to/v/7qDqGjlQe4UA9MR/Soul_Land_03_VOSTFR.mp4',
         'md5': '2bd8790b33d8e445575070774153c19f',
@@ -20,35 +15,26 @@ class StreamtapeIE(InfoExtractor):
             'id': '7qDqGjlQe4UA9MR',
             'ext': 'mp4',
             'title': 'Soul Land 03 VOSTFR.mp4',
-            'thumbnail': r're:^https?://.*\.jpg$',
             'age_limit': 18,
         },
     }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
+        PREFIX='https:/'
 
-        webpage = self._download_webpage(url, video_id)
+        html = self._download_webpage(url, video_id)
+        token = re.match(r".*document.getElementById.*\('norobotlink'\).innerHTML =.*?token=(.*?)'.*?;", html, re.M|re.S).group(1)
+        infix=re.match(r'.*<div id="ideoooolink" style="display:none;">(.*?token=).*?<[/]div>', html, re.M|re.S).group(1)
+        final_URL=f'{PREFIX}{infix}{token}'
+        orig_title=re.match(r'.*<meta name="og:title" content="(.*?)">', html, re.M|re.S).group(1)
 
-        video = self._html_search_regex(videolink, webpage, 'video', group='data')
-        video = video.split('+')
-        video = [self._parse_json(v, video_id, js_to_json) for v in video]
-        video = urljoin(url, ''.join(video))
-
-        try:
-            poster = self._html_search_regex(r' id="mainvideo"[^>]* poster="(?P<data>.*?)"',
-                                             webpage, 'poster', group='data')
-            poster = urljoin(url, poster)
-        except ValueError:
-            poster = None
-
-        title = self._og_search_title(webpage)
+        #video,title = get_curl_command(url,webpage)
 
         return {
             'id': video_id,
-            'url': video,
-            'title': title,
-            'thumbnail': poster,
+            'url': final_URL,
+            'title': orig_title,
             'age_limit': 18,
             'ext': 'mp4',
         }
